bb6f30d7ae3aeaea60bc6be62ea102f80e7b56bf
Martin Wilches <martinwilchess.dev@gmail.com>
2026-09-08 21:21:52 -0500

    feat: introducción a sql intermedio

--- 01.sql/02.intermediate.sql
+++ 01.sql/02.intermediate.sql
@@ -41,50 +41,50 @@ order by first_name desc;
 # Funciones de texto #
 ----------------------
 
--- LENGTH() - Cantidad de caracteres de una cadena de texto
+# LENGTH() - Cantidad de caracteres de una cadena de texto
 select first_name, length(first_name) from employee_demographics;
 
--- UPPER() - Convertir caracteres de un texto a mayusculas
+# UPPER() - Convertir caracteres de un texto a mayusculas
 select first_name, upper(first_name) from employee_demographics;
 
--- LOWER() - Convertir caracteres de un texto a minuscula
+# LOWER() - Convertir caracteres de un texto a minuscula
 select first_name, lower(first_name) from employee_demographics;
 
--- TRIM() - Remover espacios en blanco a la izquierda y derecha de un texto
-select trim('           sky       ');
+# TRIM() - Remover espacios en blanco a la izquierda y derecha de un texto
+select trim('    sky   ');
 
--- LTRIM() - Remover espacios a la izquierda de un texto
-select ltrim('           sky       ');
+# LTRIM() - Remover espacios a la izquierda de un texto
+select ltrim('    sky');
 
--- RTRIM() - Remover espacios a la derecha de un texto
-select rtrim('           sky       ');
+# RTRIM() - Remover espacios a la derecha de un texto
+select rtrim('sky    ');
 
--- SUBSTR() - Obtener una porción especifica de una cadena de texto
+# SUBSTR() - Obtener una porción especifica de una cadena de texto
 select first_name, substr(first_name, 1, 2) from employee_demographics;
 
--- REPLACE() - Reemplazar caracteres en una cadena de texto
+# REPLACE() - Reemplazar caracteres dentro de una cadena de texto
 select first_name, replace(first_name, 'a', 'x') from employee_demographics;
 
--- LOCATE() - Obtener la posición de caracteres en una cadena de texto
+# LOCATE() - Obtener la posición inicial de uno o varios caracteres dentro de una cadena
 select first_name, locate('rk', first_name) from employee_demographics;
 
--- CONCAT() - Unir 2 o más cadenas de texto
+# CONCAT() - Unir 2 o más cadenas de texto
 select concat(first_name, ' ', last_name) as full_name from employee_demographics;
 
 --------------------
 # Declaración CASE #
 --------------------
 
--- Estructura condicional tipo if-else
+# Estructura condicional tipo if-else
 select first_name, last_name,
-	case -- se añade una nueva columna age_label cuyo valor depende de la condición evaluada
+	case
 		when age <= 30 then 'young'
         when age between 31 and 50 then 'old'
 		when age >= 50 then 'granny'
-	end as age_label
+	end as age_label -- se añade una nueva columna `age_label` cuyo valor depende de las condiciones evaluadas
 from employee_demographics;
 
--- a partir del salario o del departamento, el salario tendra un incremento
+# a partir del salario o del departamento, el salario del empleado tendra un incremento
 select es.first_name, es.last_name, es.salary, pd.department_name,
 	case
 		when es.salary < 50000 then ((salary * 5) / 100) + salary
@@ -103,43 +103,12 @@ left join parks_departments pd on es.dept_id = pd.department_id;
 ----------------
 
 select * from employee_demographics
-where employee_id in ( -- los empleados se obtienen a partir de los ids retornados por la subconsulta a la tabla employee_salary
-	select employee_id from employee_salary where dept_id = 1
+where employee_id in (
+	select employee_id from employee_salary where dept_id = 1 -- los empleados se obtienen a partir de los ids retornados por la subconsulta a la tabla employee_salary
 );
 
--- agrupar los registros por genero, la consulta principal obtiene el promedio entre los maximos de edad obtenidos
+# La consulta principal obtiene el promedio entre las edades maximas obtenidas en la subconsulta
 select avg(max_age) from (
-	select gender, avg(age) avg_age, max(age) max_age, min(age) min_age, count(age) count_age from employee_demographics
-	group by gender
+	select gender, max(age) max_age from employee_demographics
+	group by gender -- agrupar los registros de la subconsulta por genero
 ) as agg_table;
-
-------------------------
-# Funciones de ventana #
-------------------------
-
-select dm.first_name, dm.gender,
--- Obtener el promedio de salario por genero sin colapsar las filas como ocurre con la declaración group by
-avg(salary) over(partition by gender) as rolling_avg from employee_demographics dm
-left join employee_salary sal on dm.employee_id = sal.employee_id;
-
-select dm.first_name, dm.gender, sal.salary,
--- Obtener la suma del salario por genero sin colapsar las filas
-sum(sal.salary) over(partition by gender) as rolling_total from employee_demographics dm
-left join employee_salary sal on dm.employee_id = sal.employee_id;
-
-select dm.first_name, sal.salary,
--- Obtener promedio de salario por genero sin colapsar las filas
-avg(sal.salary) over(partition by gender) as avg_salary,
--- Asignar un numero entero secuencial por cada registro de la consulta
-row_number() over(partition by dm.gender order by sal.salary desc) from employee_demographics dm
-left join employee_salary sal on dm.employee_id = sal.employee_id;
-
-select dm.first_name, salary,
--- Obtener promedio de salario por genero sin colapsar las filas
-avg(sal.salary) over(partition by gender),
--- Asignar un numero entero secuencial por cada registro de la consulta
-row_number() over(partition by gender order by sal.salary) as rolling_row_number,
--- Asignar un numero entero secuencial, si 2 registros comparten el mismo valor en la columna ordenada, se asigna el mismo numero secuencial
-rank() over(partition by gender order by sal.salary) as rolling_rank
-from employee_demographics dm
-left join employee_salary sal on sal.employee_id = dm.employee_id;
